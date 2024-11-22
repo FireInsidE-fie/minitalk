d3824b469cc944b6c96169ea4e41311381b97570
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-22 14:02:48 +0100

    add base header structure and pid printing to server

---
+++ include/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 13:48:45 by estettle          #+#    #+#             */
+/*   Updated: 2024/11/22 14:02:10 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+// Libraries
+# include <unistd.h>
+
+// Libftprintf
+# include "../libftprintf/ft_printf.h"
+
+#endif
