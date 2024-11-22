89d906f3121f0f8c69ffc81672ac041acf94f946
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-22 15:45:07 +0100

    lay out much of the plan for the server

--- src/server.c
+++ src/server.c
@@ -6,16 +6,33 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:48:45 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/22 14:01:38 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/22 15:44:53 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-int	main(void)
+void	print_received(void)
+{
+	char	received;
+
+	// Set received to the signal's value
+	ft_printf("%c", received);
+}
+
+int		main(void)
 {
 	pid_t	pid;
+	char	received;
 
 	pid = getpid();
+	received = NULL;
 	ft_printf("%d\n", pid);
+	// Set SIGUSR1 to do the data transfering
+	// Set SIGUSR2 to send back server status to the client
+	// so it knows what's going on (ready, busy, error, etc)
+
+	// Do the magic here
+
+	ft_printf("%s\n", received);
 }
