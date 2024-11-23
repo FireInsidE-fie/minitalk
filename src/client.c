e49de11cbee63a714a0637c4c109af00c11431ba
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-23 14:07:43 +0100

    I might be dumb

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 15:28:07 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/22 16:58:57 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/23 14:07:00 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(const int argc, char **argv)
 {
-	if (argc != 3)
+	if (argc != 2)
 		return (-1);
 	kill(ft_atoi(argv[1]), SIGUSR1);
 }
