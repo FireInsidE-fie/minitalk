6ab470216e5b3c974f23f920495115b9e8ba619b
Aisling <aisling.fontaine@protonmail.com>
2024-11-24 19:59:35 +0100

    switch from using signal to using sigaction

--- src/server.c
+++ src/server.c
@@ -6,13 +6,13 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:48:45 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/23 23:15:26 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/24 19:58:29 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-void	process_request(const int signum)
+static void	get_data(int signum, siginfo_t *info, void *ptr)
 {
 	/* Acknowledge the client's request by sending back SIGUSR1 (how to get the
 	 * client's pid? something about sigaction's struct?)
@@ -22,23 +22,25 @@ void	process_request(const int signum)
 	 * print the resulting UTF-8 character.
 	*/
 	(void)signum;
+	(void)info;
+	(void)ptr;
 	ft_printf("signal %d received!!\n", signum);
 	// In the end only use async safe functions like write to write the character
 }
 
 int		main(void)
 {
-	sigset_t	*mask;
-	pid_t		pid;
+	struct sigaction	minitalk;
+	sigset_t			*mask;
 
 	mask = malloc(sizeof(sigset_t));
 	if (!mask)
 		return (-1);
 	sigemptyset(mask);
-	pid = getpid();
-	ft_printf("[!] - Server PID : %d\n", pid);
-	signal(SIGUSR1, process_request);
-
+	minitalk.sa_sigaction = get_data;
+	minitalk.sa_mask = *mask;
+	ft_printf("[!] - Server PID : %d\n", getpid());
+	sigaction(SIGUSR1, &minitalk, NULL);
 	while (1)
 		sleep(100);
 }
