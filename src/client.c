7cc58034c9f841b47ebb57328ea322cd8d5cf7a4
Aisling <aisling.fontaine@protonmail.com>
2024-11-24 20:16:36 +0100

    add ping back to client functionality

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 15:28:07 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/23 14:38:16 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/24 20:11:43 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 void	process_ping(const int signum)
 {
-	ft_printf("[!] - Ping back (%d) received!", signum);
+	(void)signum;
+	write(1, "[!] - Ping back received!\n", 26);
 }
 
 int	main(const int argc, char **argv)
 {
-	signal(SIGUSR2, process_ping);
+	signal(SIGUSR1, process_ping);
 	if (argc != 2)
 		return (-1);
 	kill(ft_atoi(argv[1]), SIGUSR1);
+	sleep(1);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:48:45 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/24 19:58:29 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/24 20:09:45 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ static void	get_data(int signum, siginfo_t *info, void *ptr)
 	 * Then loop while getting the character bit by bit until it's done, and
 	 * print the resulting UTF-8 character.
 	*/
-	(void)signum;
 	(void)info;
 	(void)ptr;
+	kill(info->si_pid, SIGUSR1);
 	ft_printf("signal %d received!!\n", signum);
 	// In the end only use async safe functions like write to write the character
 }
@@ -34,9 +34,11 @@ int		main(void)
 	sigset_t			*mask;
 
 	mask = malloc(sizeof(sigset_t));
-	if (!mask)
+	if (!mask || sigemptyset(mask) != 0)
+	{
+		free(mask);
 		return (-1);
-	sigemptyset(mask);
+	}
 	minitalk.sa_sigaction = get_data;
 	minitalk.sa_mask = *mask;
 	ft_printf("[!] - Server PID : %d\n", getpid());
