4ba504d7aede935064c88fcc44b3dbbdfff7c963
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-25 09:42:49 +0100

    add signal translation to 1 and 0 in server

--- src/server.c
+++ src/server.c
@@ -6,11 +6,13 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:48:45 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/25 08:20:30 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/25 09:38:20 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
+
+int32_t	character;
 
 static void	get_data(int signum, siginfo_t *info, void *ptr)
 {
@@ -20,11 +22,12 @@ static void	get_data(int signum, siginfo_t *info, void *ptr)
 	 * Then loop while getting the character bit by bit until it's done, and
 	 * print the resulting UTF-8 character.
 	*/
-	(void)info;
 	(void)ptr;
 	kill(info->si_pid, SIGUSR1);
-	ft_printf("signal %d received!!\n", signum);
-	// In the end only use async safe functions like write to write the character
+	if (signum == SIGUSR1)
+		character = 0;
+	else
+		character = 1;
 }
 
 int		main(void)
@@ -32,14 +35,17 @@ int		main(void)
 	struct sigaction	minitalk;
 	sigset_t			mask;
 
+	character = 0;
 	sigemptyset(&mask);
 	minitalk.sa_sigaction = get_data;
 	minitalk.sa_mask = mask;
 	minitalk.sa_flags = 0;
 	ft_printf("[!] - Server PID : %d\n", getpid());
 	sigaction(SIGUSR1, &minitalk, NULL);
+	sigaction(SIGUSR2, &minitalk, NULL);
 	while (1)
-		sleep(100);
-	// Use pause() here in a loop, until a signal is received, then do some magic,
-	// then pause again
+	{
+		pause();
+		ft_printf("%d\n", character);
+	}
 }
