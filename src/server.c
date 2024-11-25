d6bc28c264f4840e6f036dfcd3d056b0885d1e89
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-25 08:27:45 +0100

    no need to malloc the sigset what was I smoking

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:48:45 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/24 20:09:45 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/25 08:20:30 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 static void	get_data(int signum, siginfo_t *info, void *ptr)
 {
-	/* Acknowledge the client's request by sending back SIGUSR1 (how to get the
-	 * client's pid? something about sigaction's struct?)
+	/*
 	 * Get the incoming character's size (by reading the first byte, the number
 	 * of 1s indicate the number of bytes used by the encoding)
 	 * Then loop while getting the character bit by bit until it's done, and
@@ -31,18 +30,16 @@ static void	get_data(int signum, siginfo_t *info, void *ptr)
 int		main(void)
 {
 	struct sigaction	minitalk;
-	sigset_t			*mask;
+	sigset_t			mask;
 
-	mask = malloc(sizeof(sigset_t));
-	if (!mask || sigemptyset(mask) != 0)
-	{
-		free(mask);
-		return (-1);
-	}
+	sigemptyset(&mask);
 	minitalk.sa_sigaction = get_data;
-	minitalk.sa_mask = *mask;
+	minitalk.sa_mask = mask;
+	minitalk.sa_flags = 0;
 	ft_printf("[!] - Server PID : %d\n", getpid());
 	sigaction(SIGUSR1, &minitalk, NULL);
 	while (1)
 		sleep(100);
+	// Use pause() here in a loop, until a signal is received, then do some magic,
+	// then pause again
 }
