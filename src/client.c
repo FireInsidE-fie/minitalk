b0a9e1285c34243bad8e800db0b9dbc92535b699
Aisling <aisling.fontaine@protonmail.com>
2024-11-25 11:48:51 +0100

    reverse the way bits get sent from the client

--- src/client.c
+++ src/client.c
@@ -6,32 +6,41 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 15:28:07 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/25 10:25:40 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/25 11:47:14 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
+
+void	ping_received(int signum)
+{
+	(void)signum;
+	ft_printf("Ping back received!!\n");
+}
 
 void	send_char(char *pid, uint32_t character)
 {
 	int	i;
 	int	target;
 
-	i = 0;
+	i = 31;
 	target = ft_atoi(pid);
-	while (i < 32)
+	ft_printf("Sent : ");
+	while (i >= 0)
 	{
-		if (((character >> i) | 1) == 0)
-			kill(SIGUSR1, target);
-		else
-			kill(SIGUSR2, target);
-		i++;
+		if (((character >> i) & 1) == 0 && ft_printf("0"))
+			kill(target,SIGUSR1);
+		else if (ft_printf("1"))
+			kill(target,SIGUSR2);
+		i--;
 	}
+	ft_printf("\n");
 }
 
 void	send_data(char *pid, char *data)
 {
-	write(1, "[!] - Ping received! Beginning data transfer...\n", 47);
+	//write(1, "[!] - Ping received! Beginning data transfer...\n", 48);
+	signal(SIGUSR1, ping_received);
 	while (*data)
 	{
 		send_char(pid, *data);
@@ -46,6 +55,8 @@ int	main(const int argc, char **argv)
 		ft_printf("Correct usage : ./client <server pid> <data to send>\n");
 		return (-1);
 	}
+	signal(SIGUSR1, ping_received);
 	send_data(argv[1], argv[2]);
+	usleep(50000);
 	return (0);
 }
