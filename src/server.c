a0d7ca1e20ebed1fddf227355521e223e934feba
Aisling <aisling.fontaine@protonmail.com>
2024-11-25 15:41:55 +0100

    add draft of character decoding to server

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:48:45 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/25 09:38:20 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:40:09 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,32 @@ int32_t	character;
 
 static void	get_data(int signum, siginfo_t *info, void *ptr)
 {
+	static uint8_t	bit_counter;
 	/*
 	 * Get the incoming character's size (by reading the first byte, the number
 	 * of 1s indicate the number of bytes used by the encoding)
 	 * Then loop while getting the character bit by bit until it's done, and
 	 * print the resulting UTF-8 character.
 	*/
+	if (!character || !bit_counter)
+	{
+		write(1, &character, 4);
+		character = 0;
+		bit_counter = 31;
+	}
 	(void)ptr;
 	kill(info->si_pid, SIGUSR1);
 	if (signum == SIGUSR1)
-		character = 0;
+	{
+		write(1, "Received 0 btw\n", 15);
+		character = (character >> bit_counter) & 1;
+	}
 	else
-		character = 1;
+	{
+		write(1, "Received 1 btw\n", 15);
+		character = (character >> bit_counter) & 0;
+	}
+	bit_counter--;
 }
 
 int		main(void)
@@ -46,6 +60,6 @@ int		main(void)
 	while (1)
 	{
 		pause();
-		ft_printf("%d\n", character);
+		//ft_printf("%d\n", character);
 	}
 }
