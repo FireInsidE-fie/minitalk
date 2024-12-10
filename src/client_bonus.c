3e80914241962e712d61c6ff1de00ae89fc2cce0
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-12-10 12:28:08 +0100

    add bonus c files

---
+++ src/client_bonus.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 15:28:07 by estettle          #+#    #+#             */
+/*   Updated: 2024/12/10 11:30:52 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minitalk.h"
+
+void	handle_ping_back(int signum)
+{
+	(void)signum;
+}
+
+void	send_data(const char *pid, const char *data)
+{
+	int	i;
+	int	target;
+
+	while (*data)
+	{
+		i = 31;
+		target = ft_atoi(pid);
+		while (i >= 0)
+		{
+			if (((*data >> i--) & 1) == 0)
+				kill(target, SIGUSR1);
+			else
+				kill(target, SIGUSR2);
+			pause();
+			usleep(25);
+		}
+		data++;
+	}
+}
+
+int	main(const int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		ft_printf("Correct usage : ./client <server pid> <data to send>\n");
+		return (-1);
+	}
+	signal(SIGUSR1, handle_ping_back);
+	send_data(argv[1], argv[2]);
+	return (0);
+}
---
+++ src/server_bonus.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 13:48:45 by estettle          #+#    #+#             */
+/*   Updated: 2024/12/10 11:37:12 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+#include "../include/minitalk.h"
+
+int32_t	g_character;
+
+static void	get_data(int signum, siginfo_t *info, void *ptr)
+{
+	static int8_t	bit_counter;
+
+	if (g_character < 0)
+	{
+		g_character = 0;
+		bit_counter = 31;
+	}
+	(void)ptr;
+	if (signum == SIGUSR1)
+		g_character = (g_character << 1) | 0;
+	else
+		g_character = (g_character << 1) | 1;
+	if (--bit_counter < 0)
+	{
+		write(1, &g_character, 1);
+		g_character = 0;
+		bit_counter = 31;
+	}
+	usleep(25);
+	kill(info->si_pid, SIGUSR1);
+}
+
+int	main(void)
+{
+	struct sigaction	minitalk;
+	sigset_t			mask;
+
+	g_character = -1;
+	sigemptyset(&mask);
+	minitalk.sa_sigaction = get_data;
+	minitalk.sa_mask = mask;
+	minitalk.sa_flags = SA_SIGINFO;
+	ft_printf("[!] - Server PID : %d\n", getpid());
+	sigaction(SIGUSR1, &minitalk, NULL);
+	sigaction(SIGUSR2, &minitalk, NULL);
+	while (1)
+		pause();
+}
